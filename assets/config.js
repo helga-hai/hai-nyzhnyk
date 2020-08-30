9136e7178bc9b6fa6298c78da7fdb1de0da60df5
helga-hai <o.hainyzhnyk@gmail.com>
2020-08-30 11:59:31 +0300

    url

--- assets/config.js
+++ assets/config.js
@@ -13,7 +13,7 @@ export default {
     // these are the rest api endpoints and your wordpress url 
     client: `https://${apiEndpoint}`,
     wpDomain: `https://${apiEndpoint}/wp-json`,
-    url: 'https://nuxt-headless.netlify.com/', // your website url
+    url: 'https://hai-nyzhnyk.herokuapp.com/', //'https://nuxt-headless.netlify.com/', // your website url
     loadDbName: '[starter_wp]', // db name if needed
     api: {
         homePage: '/wp/v2/pages/8', // the [page_id] from WordPress
